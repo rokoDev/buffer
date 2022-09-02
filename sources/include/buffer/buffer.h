dfcd5aabc2e1ab56fdd646e65e47477bf8a05975
rokoDev <mishaDoter@gmail.com>
2022-09-03 00:43:14 +0300

    implemented new methods and trigger ci build for s390x

--- sources/include/buffer/buffer.h
+++ sources/include/buffer/buffer.h
@@ -1,43 +1,134 @@
 #ifndef buffer_h
 #define buffer_h
 
+#include <array>
+#include <boost/leaf.hpp>
 #include <cstddef>
+#include <string>
+#include <string_view>
 #include <type_traits>
+#include <vector>
 
 namespace buffer
 {
+namespace leaf = boost::leaf;
+
+template <class T>
+using result = leaf::result<T>;
+
+enum class error
+{
+    null_data_pointer = 1,
+    zero_size,
+    null_data_and_zero_size,
+    invalid_index,
+    unknown
+};
+
 namespace details
 {
-template <typename DataT, bool kConstFlag>
+template <typename T>
+struct has_contiguous_storage : public std::false_type
+{
+};
+
+template <typename T>
+struct has_contiguous_storage<std::vector<T>> : public std::true_type
+{
+};
+
+template <>
+struct has_contiguous_storage<std::vector<bool>> : public std::false_type
+{
+};
+
+template <typename T, std::size_t N>
+struct has_contiguous_storage<std::array<T, N>> : public std::true_type
+{
+};
+
+template <typename T>
+struct has_contiguous_storage<
+    std::basic_string<T, std::char_traits<T>, std::allocator<T>>>
+    : public std::true_type
+{
+};
+
+template <typename T>
+struct has_contiguous_storage<std::basic_string_view<T, std::char_traits<T>>>
+    : public std::true_type
+{
+};
+
+template <typename T, bool isConst>
+class buffer_view_base;
+
+template <typename T, bool isConst>
+struct has_contiguous_storage<buffer_view_base<T, isConst>>
+    : public std::true_type
+{
+};
+
+template <typename T>
+inline constexpr bool has_contiguous_storage_v =
+    has_contiguous_storage<T>::value;
+
+template <typename T>
+result<void> validate_args(T aData, std::size_t aSize) noexcept
+{
+    if (aData && aSize)
+    {
+        return {};
+    }
+    if (!aData && !aSize)
+    {
+        return leaf::new_error(error::null_data_and_zero_size);
+    }
+    if (!aData)
+    {
+        return leaf::new_error(error::null_data_pointer);
+    }
+    else
+    {
+        return leaf::new_error(error::zero_size);
+    }
+}
+
+template <typename T, bool isConst>
 class buffer_view_base
 {
+    friend buffer_view_base<T, not isConst>;
+
    public:
-    using pointer = std::add_pointer_t<
-        std::conditional_t<kConstFlag, std::add_const_t<DataT>, DataT>>;
-    using value_type = DataT;
-    static constexpr bool kIsConst = kConstFlag;
+    using pointer =
+        std::add_pointer_t<std::conditional_t<isConst, std::add_const_t<T>, T>>;
+    using value_type = T;
+
+    static result<buffer_view_base<T, isConst>> create(
+        pointer aDataPtr, std::size_t aSize) noexcept
+    {
+        auto r = validate_args(aDataPtr, aSize);
+        if (r)
+        {
+            return buffer_view_base<T, isConst>(aDataPtr, aSize);
+        }
+        return r.error();
+    }
 
     ~buffer_view_base() = default;
 
-    constexpr buffer_view_base() noexcept = delete;
+    buffer_view_base() noexcept = delete;
 
-    constexpr buffer_view_base(pointer aData, std::size_t aSize) noexcept
-        : data_(aData), size_(aSize)
+    constexpr operator buffer_view_base<T, true>() const
     {
+        return buffer_view_base<T, true>(data_, size_);
     }
 
-    template <std::size_t N, typename T>
-    constexpr explicit buffer_view_base(T (&aData)[N]) noexcept
-        : buffer_view_base(aData, N)
-    {
-    }
-
-    inline constexpr pointer data_at_unsafe(std::size_t aIndex) const noexcept
+    inline constexpr pointer data_unsafe(std::size_t aIndex) const noexcept
     {
         if constexpr (std::is_same_v<value_type, void>)
         {
-            using CharPtrT =
-                std::conditional_t<kConstFlag, char const *, char *>;
+            using CharPtrT = std::conditional_t<isConst, char const *, char *>;
             return static_cast<CharPtrT>(data_) + aIndex;
         }
         else
@@ -46,21 +137,145 @@ class buffer_view_base
         }
     }
 
+    inline result<pointer> data(std::size_t aIndex) const noexcept
+    {
+        if (aIndex < size_)
+        {
+            return data_unsafe(aIndex);
+        }
+        return leaf::new_error(error::invalid_index);
+    }
+
+    inline result<value_type> operator[](std::size_t aIndex) const noexcept
+    {
+        result<pointer> r = data(aIndex);
+        if (r)
+        {
+            return *(r.value());
+        }
+        else
+        {
+            return r.error();
+        }
+    }
+
     inline constexpr pointer data() const noexcept { return data_; }
 
     inline constexpr std::size_t size() const noexcept { return size_; }
 
    private:
+    inline constexpr buffer_view_base(pointer aData, std::size_t aSize) noexcept
+        : data_(aData), size_(aSize)
+    {
+    }
+
     pointer data_{};
     std::size_t size_{};
 };
 }  // namespace details
 
 template <typename T>
+using buffer_view = details::buffer_view_base<T, false>;
+
+template <typename T>
 using buffer_view_const = details::buffer_view_base<T, true>;
 
 template <typename T>
-using buffer_view = details::buffer_view_base<T, false>;
+struct is_bv : public std::false_type
+{
+};
+
+template <typename T>
+struct is_bv<buffer_view<T>> : public std::true_type
+{
+};
+
+template <typename T>
+inline constexpr bool is_bv_v = is_bv<T>::value;
+
+template <typename T>
+struct is_bv_const : public std::false_type
+{
+};
+
+template <typename T>
+struct is_bv_const<buffer_view_const<T>> : public std::true_type
+{
+};
+
+template <typename T>
+inline constexpr bool is_bv_const_v = is_bv_const<T>::value;
+
+template <typename T, typename DataT = std::remove_pointer_t<T>>
+inline result<buffer_view<DataT>> make_bv(T aDataPtr,
+                                          std::size_t aSize) noexcept
+{
+    static_assert(not std::is_const_v<DataT>,
+                  "aDataPtr parameter should be pointer to non const data.");
+    return buffer_view<DataT>::create(aDataPtr, aSize);
+}
+
+template <typename T,
+          typename DataT = std::remove_const_t<std::remove_pointer_t<T>>>
+inline result<buffer_view_const<DataT>> make_bv_const(
+    T aDataPtr, std::size_t aSize) noexcept
+{
+    using PointerT = std::add_pointer_t<std::add_const_t<DataT>>;
+    return buffer_view_const<DataT>::create(static_cast<PointerT>(aDataPtr),
+                                            aSize);
+}
+
+template <typename T, std::size_t N>
+inline auto make_bv(T (&aData)[N]) noexcept
+{
+    return make_bv(aData, N);
+}
+
+template <typename T, std::size_t N>
+inline auto make_bv_const(T (&aData)[N]) noexcept
+{
+    return make_bv_const(aData, N);
+}
+
+template <typename T>
+inline auto make_bv(T &&aParam) noexcept
+{
+    using U = std::remove_cv_t<std::remove_reference_t<T>>;
+    if constexpr (is_bv_v<U>)
+    {
+        return make_bv(aParam.data(), aParam.size());
+    }
+    else
+    {
+        static_assert(std::is_lvalue_reference_v<T>,
+                      "aContainer's parameter type must be lvalue reference.");
+        static_assert(
+            details::has_contiguous_storage_v<std::remove_reference_t<T>>,
+            "aContainer must have contiguous storage.");
+        static_assert(not std::is_const_v<T>,
+                      "aContainer parameter must be non const.");
+        return make_bv(aParam.data(), aParam.size());
+    }
+}
+
+template <typename T>
+inline auto make_bv_const(T &&aParam) noexcept
+{
+    using U = std::remove_cv_t<std::remove_reference_t<T>>;
+    if constexpr (is_bv_v<U> || is_bv_const_v<U>)
+    {
+        return make_bv_const(aParam.data(), aParam.size());
+    }
+    else
+    {
+        static_assert(std::is_lvalue_reference_v<T>,
+                      "aContainer's parameter type must be lvalue reference.");
+        static_assert(details::has_contiguous_storage_v<
+                          std::remove_cv_t<std::remove_reference_t<T>>>,
+                      "aContainer must have contiguous storage.");
+        return make_bv_const(aParam.data(), aParam.size());
+    }
+}
 }  // namespace buffer
 
 #endif /* buffer_h */
--- tests/src/buffer_tests.cpp
+++ tests/src/buffer_tests.cpp
@@ -3,8 +3,114 @@
 #include <type_name/type_name.h>
 
 #include <array>
+#include <type_traits>
 
 using namespace std::literals::string_view_literals;
+
+namespace leaf = boost::leaf;
+
+template <class T>
+using result = leaf::result<T>;
+
+template <typename E>
+constexpr
+    typename std::enable_if_t<std::is_enum_v<E>, std::underlying_type_t<E>>
+    to_underlying(E e) noexcept
+{
+    return static_cast<typename std::underlying_type_t<E>>(e);
+}
+
+template <typename BufT>
+constexpr void validateGeneralBufferTypeProperties() noexcept
+{
+    static_assert(not std::is_default_constructible_v<BufT>,
+                  "BufT supposed to be not default constructible.");
+    static_assert(not std::is_trivially_default_constructible_v<BufT>,
+                  "BufT supposed to be not trivially default constructible.");
+    static_assert(not std::is_nothrow_default_constructible_v<BufT>,
+                  "BufT supposed to be not nothrow default constructible");
+
+    static_assert(std::is_trivially_copy_constructible_v<BufT>,
+                  "BufT must be trivially copy constructible.");
+    static_assert(std::is_nothrow_copy_constructible_v<BufT>,
+                  "BufT must be nothrow copy constructible.");
+
+    static_assert(std::is_trivially_move_constructible_v<BufT>,
+                  "BufT must be trivially move constructible.");
+    static_assert(std::is_nothrow_move_constructible_v<BufT>,
+                  "BufT must be nothrow move constructible.");
+
+    static_assert(std::is_trivially_assignable_v<BufT, BufT>,
+                  "BufT must be trivially assignable to BufT.");
+    static_assert(std::is_nothrow_assignable_v<BufT, BufT>,
+                  "BufT must be nothrow assignable to BufT.");
+
+    static_assert(std::is_trivially_copy_assignable_v<BufT>,
+                  "BufT must be trivially copy assignable.");
+    static_assert(std::is_nothrow_copy_assignable_v<BufT>,
+                  "BufT must be nothrow copy assignable.");
+
+    static_assert(std::is_trivially_move_assignable_v<BufT>,
+                  "BufT must be trivially move assignable.");
+    static_assert(std::is_nothrow_move_assignable_v<BufT>,
+                  "BufT must be nothrow move assignable.");
+
+    static_assert(std::is_trivially_destructible_v<BufT>,
+                  "BufT must be trivially destructible.");
+    static_assert(std::is_nothrow_destructible_v<BufT>,
+                  "BufT must be nothrow destructible.");
+
+    static_assert(not std::has_virtual_destructor_v<BufT>,
+                  "BufT must have no virtual destructor.");
+}
+
+namespace test_utils
+{
+template <typename CallableT>
+int execute(CallableT &&aArg) noexcept
+{
+    const auto r = leaf::try_handle_all(
+        aArg,
+        [](leaf::match<buffer::error, buffer::error::null_data_and_zero_size>)
+        { return to_underlying(buffer::error::null_data_and_zero_size); },
+        [](leaf::match<buffer::error, buffer::error::null_data_pointer>)
+        { return to_underlying(buffer::error::null_data_pointer); },
+        [](leaf::match<buffer::error, buffer::error::zero_size>)
+        { return to_underlying(buffer::error::zero_size); },
+        [](leaf::match<buffer::error, buffer::error::invalid_index>)
+        { return to_underlying(buffer::error::invalid_index); },
+        [](leaf::error_info const &unmatched)
+        {
+            std::cerr << "Unknown failure detected" << std::endl
+                      << "Cryptic diagnostic information follows" << std::endl
+                      << unmatched;
+            return to_underlying(buffer::error::unknown);
+        });
+    return r;
+}
+}  // namespace test_utils
+
+#ifdef BOOST_LEAF_NO_EXCEPTIONS
+
+namespace boost
+{
+[[noreturn]] void throw_exception(std::exception const &e)
+{
+    std::cerr
+        << "Terminating due to a C++ exception under BOOST_LEAF_NO_EXCEPTIONS: "
+        << e.what();
+    std::terminate();
+}
+
+struct source_location;
+[[noreturn]] void throw_exception(std::exception const &e,
+                                  boost::source_location const &)
+{
+    throw_exception(e);
+}
+}  // namespace boost
+
+#endif
 
 TEST(BufferViewConst, ConstructorTest1)
 {
@@ -13,13 +119,13 @@ TEST(BufferViewConst, ConstructorTest1)
     constexpr std::size_t kSize = 10;
     std::array<DataT, kSize> array{};
     using Buf = buffer::buffer_view_const<BufDataT>;
-    auto buf = Buf(array.data(), array.size());
+    result<Buf> buf = buffer::make_bv_const(array.data(), array.size());
     static_assert(type_name::type_name<Buf::value_type>() == "char"sv,
                   "Invalid value_type.");
     static_assert(type_name::type_name<Buf::pointer>() == "const char *"sv,
                   "Invalid value_type.");
-    ASSERT_EQ(buf.size(), kSize);
-    ASSERT_EQ(buf.data(), array.data());
+    ASSERT_EQ(buf.value().size(), kSize);
+    ASSERT_EQ(buf.value().data(), array.data());
 }
 
 TEST(BufferViewConst, ConstructorTest2)
@@ -29,13 +135,14 @@ TEST(BufferViewConst, ConstructorTest2)
     constexpr std::size_t kSize = 10;
     const std::array<DataT, kSize> array{};
     using Buf = buffer::buffer_view_const<BufDataT>;
-    auto buf = Buf(array.data(), array.size());
+    result<Buf> buf = buffer::make_bv_const(
+        static_cast<void const *>(array.data()), array.size());
     static_assert(type_name::type_name<Buf::value_type>() == "void"sv,
                   "Invalid value_type.");
     static_assert(type_name::type_name<Buf::pointer>() == "const void *"sv,
                   "Invalid value_type.");
-    ASSERT_EQ(buf.size(), kSize);
-    ASSERT_EQ(buf.data(), array.data());
+    ASSERT_EQ(buf.value().size(), kSize);
+    ASSERT_EQ(buf.value().data(), array.data());
 }
 
 TEST(BufferViewConst, ConstructorTest3)
@@ -45,28 +152,181 @@ TEST(BufferViewConst, ConstructorTest3)
     constexpr std::size_t kSize = 10;
     const std::array<DataT, kSize> array{};
     using Buf = buffer::buffer_view_const<BufDataT>;
-    auto buf = Buf(array.data(), array.size());
+    result<Buf> buf = buffer::make_bv_const(array.data(), array.size());
     static_assert(type_name::type_name<Buf::value_type>() == "short"sv,
                   "Invalid value_type.");
     static_assert(type_name::type_name<Buf::pointer>() == "const short *"sv,
                   "Invalid value_type.");
-    ASSERT_EQ(buf.size(), kSize);
-    ASSERT_EQ(buf.data(), array.data());
+    ASSERT_EQ(buf.value().size(), kSize);
+    ASSERT_EQ(buf.value().data(), array.data());
 }
 
-TEST(BufferViewConst, ConstructorWithCArrayTest1)
+TEST(BufferViewConst, ConstructFromCArrayTest1)
 {
     using DataT = unsigned char;
     using BufDataT = unsigned char;
     constexpr std::size_t kSize = 10;
-    DataT array[kSize]{};
+    const DataT array[kSize]{};
     using Buf = buffer::buffer_view_const<BufDataT>;
-    auto buf = Buf(array);
+    result<Buf> buf = buffer::make_bv_const(array);
     static_assert(type_name::type_name<Buf::value_type>() == "unsigned char"sv,
                   "Invalid value_type.");
     static_assert(
         type_name::type_name<Buf::pointer>() == "const unsigned char *"sv,
         "Invalid value_type.");
-    ASSERT_EQ(buf.size(), kSize);
-    ASSERT_EQ(buf.data(), array);
+    ASSERT_EQ(buf.value().size(), kSize);
+    ASSERT_EQ(buf.value().data(), array);
+}
+
+TEST(BufferViewConst, ConstructFromSTDArrayTest)
+{
+    using DataT = unsigned char;
+    using BufDataT = unsigned char;
+    constexpr std::size_t kSize = 10;
+    const std::array<DataT, kSize> array{};
+    using BufT = buffer::buffer_view_const<BufDataT>;
+    result<BufT> buf = buffer::make_bv_const(array);
+    static_assert(type_name::type_name<BufT::value_type>() == "unsigned char"sv,
+                  "Invalid value_type.");
+    static_assert(
+        type_name::type_name<BufT::pointer>() == "const unsigned char *"sv,
+        "Invalid value_type.");
+    ASSERT_EQ(buf.value().size(), kSize);
+    ASSERT_EQ(buf.value().data(), array.data());
+}
+
+TEST(BufferView, ConstructorTest1)
+{
+    using DataT = int16_t;
+    using BufDataT = int16_t;
+    constexpr std::size_t kSize = 10;
+    std::array<DataT, kSize> array{};
+    using Buf = buffer::buffer_view<BufDataT>;
+    result<Buf> buf = buffer::make_bv(array.data(), array.size());
+    static_assert(type_name::type_name<Buf::value_type>() == "short"sv,
+                  "Invalid value_type.");
+    static_assert(type_name::type_name<Buf::pointer>() == "short *"sv,
+                  "Invalid value_type.");
+    ASSERT_EQ(buf.value().size(), kSize);
+    ASSERT_EQ(buf.value().data(), array.data());
+}
+
+TEST(BufferView, ConstructFromCArrayTest1)
+{
+    using DataT = unsigned char;
+    using BufDataT = unsigned char;
+    constexpr std::size_t kSize = 10;
+    DataT array[kSize]{};
+    using Buf = buffer::buffer_view<BufDataT>;
+    result<Buf> buf = buffer::make_bv(array);
+    static_assert(type_name::type_name<Buf::value_type>() == "unsigned char"sv,
+                  "Invalid value_type.");
+    static_assert(type_name::type_name<Buf::pointer>() == "unsigned char *"sv,
+                  "Invalid value_type.");
+    ASSERT_EQ(buf.value().size(), kSize);
+    ASSERT_EQ(buf.value().data(), array);
+}
+
+TEST(BufferView, ConstructFromSTDArrayTest)
+{
+    using DataT = unsigned char;
+    using BufDataT = unsigned char;
+    constexpr std::size_t kSize = 10;
+    std::array<DataT, kSize> array{};
+    using BufT = buffer::buffer_view<BufDataT>;
+    result<BufT> buf = buffer::make_bv(array);
+    static_assert(type_name::type_name<BufT::value_type>() == "unsigned char"sv,
+                  "Invalid value_type.");
+    static_assert(type_name::type_name<BufT::pointer>() == "unsigned char *"sv,
+                  "Invalid value_type.");
+    ASSERT_EQ(buf.value().size(), kSize);
+    ASSERT_EQ(buf.value().data(), array.data());
+}
+
+TEST(BufferView, ValidateTypeProperties)
+{
+    using DataT = unsigned char;
+    using BufT = buffer::buffer_view<DataT>;
+    validateGeneralBufferTypeProperties<BufT>();
+}
+
+TEST(BufferViewConst, ValidateTypeProperties)
+{
+    using DataT = unsigned char;
+    using BufT = buffer::buffer_view<DataT>;
+    using ConstBufT = buffer::buffer_view_const<DataT>;
+    validateGeneralBufferTypeProperties<BufT>();
+    static_assert(std::is_convertible_v<BufT, ConstBufT>,
+                  "BufT must be convertible to ConstBufT.");
+}
+
+TEST(BufferView, ConstructFromInvalidDataAndSize)
+{
+    const auto r = test_utils::execute(
+        [&]() -> result<int>
+        {
+            using DataT = unsigned char;
+            using DataPtrT = DataT *;
+            DataPtrT dataPtr = nullptr;
+            constexpr std::size_t kSize = 0;
+            using BufT = buffer::buffer_view<DataT>;
+            result<BufT> buf = buffer::make_bv(dataPtr, kSize);
+            return buf.has_error() ? buf.error() : result<int>{0};
+        });
+
+    ASSERT_EQ(r, to_underlying(buffer::error::null_data_and_zero_size));
+}
+
+TEST(BufferView, ConstructFromNullDataPointer)
+{
+    const auto r = test_utils::execute(
+        [&]() -> result<int>
+        {
+            using DataT = unsigned char;
+            using DataPtrT = DataT *;
+            DataPtrT dataPtr = nullptr;
+            constexpr std::size_t kSize = 10;
+            using BufT = buffer::buffer_view<DataT>;
+            result<BufT> buf = buffer::make_bv(dataPtr, kSize);
+            return buf.has_error() ? buf.error() : result<int>{0};
+        });
+
+    ASSERT_EQ(r, to_underlying(buffer::error::null_data_pointer));
+}
+
+TEST(BufferView, ConstructFromZeroSize)
+{
+    const auto r = test_utils::execute(
+        [&]() -> result<int>
+        {
+            using DataT = unsigned char;
+            constexpr std::size_t kSize = 10;
+            DataT rawPtr[kSize]{};
+            using BufT = buffer::buffer_view<DataT>;
+            result<BufT> buf = buffer::make_bv(rawPtr, 0);
+            return buf.has_error() ? buf.error() : result<int>{0};
+        });
+
+    ASSERT_EQ(r, to_underlying(buffer::error::zero_size));
+}
+
+TEST(BufferView, AccessByInvalidIndex)
+{
+    const auto r = test_utils::execute(
+        [&]() -> result<int>
+        {
+            using DataT = unsigned char;
+            constexpr std::size_t kSize = 10;
+            DataT rawBuf[kSize]{};
+            using BufT = buffer::buffer_view<DataT>;
+            result<BufT> buf = buffer::make_bv(rawBuf);
+            if (buf)
+            {
+                auto value = buf.value()[kSize];
+                return value.has_error() ? value.error() : result<int>{};
+            }
+            return buf.error();
+        });
+
+    ASSERT_EQ(r, to_underlying(buffer::error::invalid_index));
 }
